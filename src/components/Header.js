549e3f2dfa0378ce31093156098630fe8a9aad15
Daijiro Wachi <daijiro.wachi@gmail.com>
2021-08-05 13:56:23 +0900

    feat: integrate drawer and report

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,37 @@
 import React from "react";
+import {
+  Grid,
+  Typography,
+  AppBar,
+  Drawer,
+  Hidden,
+  IconButton,
+  Toolbar,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Box,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import {
-  AppBar,
-  Container,
-  Grid,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
+  Menu as MenuIcon,
+  Event as EventIcon,
+  Description as DescriptionIcon,
+  Business as BusinessIcon,
+  School as SchoolIcon,
+  Close as CloseIcon,
+} from "@material-ui/icons";
 import Link from "./Link";
-import EventIcon from "@material-ui/icons/Event";
-import DescriptionIcon from "@material-ui/icons/Description";
 import LogoIcon from "../images/logo.svg";
-import BusinessIcon from "@material-ui/icons/Business";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
   icon: {
     verticalAlign: "middle",
   },
@@ -23,21 +41,102 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     fontSize: "1.1rem",
+    fontWeight: "bold",
   },
-  mobile: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  menuButton: {
+    [theme.breakpoints.up("lg")]: {
+      display: "none",
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  inline: {
+    [theme.breakpoints.up("lg")]: {
       display: "inline",
     },
   },
+  spacer: {
+    marginBottom: "64px",
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "56px",
+    },
+  },
 }));
-export default function Header() {
+
+export default function Header(props) {
+  const { window } = props;
   const classes = useStyles();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  const ResponsiveDrawer = (
+    <>
+      <List>
+        <ListItem onClick={handleDrawerToggle} button>
+          <ListItemIcon>
+            <CloseIcon />
+          </ListItemIcon>
+        </ListItem>
+        <Divider />
+        <Link to="/events">
+          <ListItem button>
+            <ListItemIcon>
+              <EventIcon />
+            </ListItemIcon>
+            <ListItemText primary="イベント" />
+          </ListItem>
+        </Link>
+        <Link to="/resources">
+          <ListItem button>
+            <ListItemIcon>
+              <SchoolIcon />
+            </ListItemIcon>
+            <ListItemText primary="学習リソース" />
+          </ListItem>
+        </Link>
+        <Link to="/companies">
+          <ListItem button>
+            <ListItemIcon>
+              <BusinessIcon />
+            </ListItemIcon>
+            <ListItemText primary="利用企業" />
+          </ListItem>
+        </Link>
+        <Link to="/reports">
+          <ListItem button>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            <ListItemText primary="レポート" />
+          </ListItem>
+        </Link>
+      </List>
+    </>
+  );
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Container>
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
           <Grid container justifyContent="space-between" alignItems="center">
             <Grid item>
               <Typography variant="h6" className={classes.title}>
@@ -49,9 +148,13 @@ export default function Header() {
                     src={LogoIcon}
                   />
                   &nbsp;
-                  <span className={classes.mobile}>
-                    Amplify 日本ユーザーグループ
-                  </span>
+                  <Hidden
+                    smDown
+                    implementation="css"
+                    className={classes.inline}
+                  >
+                    Amplify Japan User Group
+                  </Hidden>
                   <span role="img" aria-label="日本国旗">
                     🇯🇵
                   </span>
@@ -59,35 +162,62 @@ export default function Header() {
               </Typography>
             </Grid>
             <Grid item>
-              <Grid
-                container
-                justifyContent="flex-end"
-                alignItems="center"
-                spacing={1}
-              >
-                <Grid item>
-                  <Link to="/events">
-                    <EventIcon className={classes.icon} />
-                    イベント
-                  </Link>
+              <Hidden mdDown implementation="css">
+                <Grid
+                  container
+                  justifyContent="flex-end"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Grid item>
+                    <Link to="/events">
+                      <EventIcon className={classes.icon} />
+                      イベント
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link to="/resources">
+                      <SchoolIcon className={classes.icon} />
+                      リソース
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link to="/reports">
+                      <DescriptionIcon className={classes.icon} />
+                      レポート
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link to="/companies" color="inherit">
+                      <BusinessIcon className={classes.icon} />
+                      利用企業
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link to="/resources">
-                    <DescriptionIcon className={classes.icon} />
-                    リソース
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link to="/companies" color="inherit">
-                    <BusinessIcon className={classes.icon} />
-                    利用企業
-                  </Link>
-                </Grid>
-              </Grid>
+              </Hidden>
             </Grid>
           </Grid>
-        </Container>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      <Hidden lgUp implementation="css">
+        <nav className={classes.drawer}>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true,
+            }}
+          >
+            {ResponsiveDrawer}
+          </Drawer>
+        </nav>
+      </Hidden>
+      <Box className={classes.spacer} />
+    </div>
   );
 }
---
+++ src/contents/reports/20210801-amplify-boost-up-01.md
@@ -0,0 +1,36 @@
+---
+slug: "/reports/20210801-amplify-boost-up-01"
+title: "Amplify Boost Up #01"
+date: "2021.07.30"
+author: "Daijiro Wachi"
+---
+
+# Amplify Boost Up #01
+
+2021/07/30 に開催された Amplify Boost Up #01 のコンテンツまとめとなります。
+
+## アーカイブ
+
+- https://www.youtube.com/watch?v=nc4b-FTEnCk
+
+## Amplify Boost Up by Amplify Japan User Group 紹介資料
+
+- 発表者：[@tacck](https://twitter.com/tacck)
+- 資料：https://speakerdeck.com/tacck/amplify-boost-up-by-amplify-japan-user-group-shao-jie-zi-liao-number-awsamplifyjp
+
+## Amazon Location Service と MapLibre GL JS と AWS Amplify と Vue.js を組み合わせてマップアプリケーションを構築してみた
+
+- 発表者：[Yasunori Kirimoto](https://twitter.com/dayjournal_nori)
+- 資料：https://docs.google.com/presentation/d/e/2PACX-1vS6nCeulMm_paP0osUpnHTyLAMQpjgGBdCTvv5Ao_Hs58WbNM2eEmgkl-5sGNniF5HKjs2bPML183rx/pub?start=false&loop=false&delayms=3000&slide=id.ge2d173d3ec_0_90
+
+## Amplify でいろんな機能拡張をしていったお話
+
+- 発表者：[髙田健太郎(ゲバラ)](https://twitter.com/POL53297840)
+- 資料：https://speakerdeck.com/guevara001/amplifydeironnaji-neng-kuo-zhang-wositeitutaohua
+
+## amplify-cli で追加した function に環境変数・シークレットを設定する機能がどのように実現されたか
+
+- 発表者：[@watilde](https://twitter.com/watilde)
+- 資料：https://speakerdeck.com/watilde/amplify-env-vars
+
+<to-report-index />
